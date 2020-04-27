7ae62d32124ea2388db6cbef0eeb6fc6ed196af9
tuxxy <me@johnpacific.com>
2020-04-27 12:40:45 +0200

    Implement basic field arithmetic for secp256k1
    This introduces changes made by @str4d for p256, but modifies them for
    secp256k1.
    
    Additionally, this implementation primarily uses montgomery multiplication
    rather than schoolbook multiplication + a montgomery reduction for added
    performance gains. However, it lacks a more efficient squaring
    implementation.
    
    Make clippy less angry

---
+++ k256/src/arithmetic.rs
@@ -0,0 +1,201 @@
+//! A pure-Rust implementation of group operations on secp256k1.
+//!
+//! # Status
+//!
+//! Currently, no actual group operations are implemented. Only point compression and
+//! decompression is supported.
+
+mod field;
+mod util;
+
+#[cfg(any(feature = "test-vectors", test))]
+pub mod test_vectors;
+
+use core::convert::TryInto;
+use elliptic_curve::generic_array::GenericArray;
+use subtle::{Choice, ConditionallySelectable, ConstantTimeEq, CtOption};
+
+use super::{PublicKey, Secp256k1};
+use elliptic_curve::weierstrass::{CompressedCurvePoint, UncompressedCurvePoint};
+use field::{FieldElement, MODULUS};
+
+/// b = 7
+const CURVE_EQUATION_B: FieldElement = FieldElement::one()
+    .add(&FieldElement::one())
+    .add(&FieldElement::one())
+    .add(&FieldElement::one())
+    .add(&FieldElement::one())
+    .add(&FieldElement::one())
+    .add(&FieldElement::one());
+
+/// A point on the secp256k1 curve in affine coordinates.
+#[derive(Clone, Copy, Debug)]
+pub struct AffinePoint {
+    x: FieldElement,
+    y: FieldElement,
+}
+
+impl ConditionallySelectable for AffinePoint {
+    fn conditional_select(a: &AffinePoint, b: &AffinePoint, choice: Choice) -> AffinePoint {
+        AffinePoint {
+            x: FieldElement::conditional_select(&a.x, &b.x, choice),
+            y: FieldElement::conditional_select(&a.y, &b.y, choice),
+        }
+    }
+}
+
+impl ConstantTimeEq for AffinePoint {
+    fn ct_eq(&self, other: &AffinePoint) -> Choice {
+        self.x.ct_eq(&other.x) & self.y.ct_eq(&other.y)
+    }
+}
+
+impl PartialEq for AffinePoint {
+    fn eq(&self, other: &AffinePoint) -> bool {
+        self.ct_eq(other).into()
+    }
+}
+
+impl Eq for AffinePoint {}
+
+impl AffinePoint {
+    /// Attempts to parse the given [`PublicKey`] as an SEC-1-encoded `AffinePoint`.
+    ///
+    /// # Returns
+    ///
+    /// `None` value if `pubkey` is not on the secp256k1 curve.
+    pub fn from_pubkey(pubkey: &PublicKey) -> CtOption<Self> {
+        match pubkey {
+            PublicKey::Compressed(point) => {
+                let bytes = point.as_bytes();
+
+                let y_is_odd = Choice::from(bytes[0] & 0x01);
+                let x = FieldElement::from_bytes(bytes[1..33].try_into().unwrap());
+
+                x.and_then(|x| {
+                    let alpha = (x * &x * &x) + &CURVE_EQUATION_B;
+                    let beta = alpha.sqrt();
+
+                    beta.map(|beta| {
+                        let y = FieldElement::conditional_select(
+                            &(MODULUS - &beta),
+                            &beta,
+                            // beta.is_odd() == y_is_odd
+                            !(beta.is_odd() ^ y_is_odd),
+                        );
+
+                        AffinePoint { x, y }
+                    })
+                })
+            }
+            PublicKey::Uncompressed(point) => {
+                let bytes = point.as_bytes();
+
+                let x = FieldElement::from_bytes(bytes[1..33].try_into().unwrap());
+                let y = FieldElement::from_bytes(bytes[33..65].try_into().unwrap());
+
+                x.and_then(|x| {
+                    y.and_then(|y| {
+                        // Check that the point is on the curve
+                        let lhs = y * &y;
+                        let rhs = x * &x * &x + &CURVE_EQUATION_B;
+                        CtOption::new(AffinePoint { x, y }, lhs.ct_eq(&rhs))
+                    })
+                })
+            }
+        }
+    }
+
+    /// Returns the SEC-1 compressed encoding of this point.
+    pub fn to_compressed_pubkey(&self) -> CompressedCurvePoint<Secp256k1> {
+        let mut encoded = [0; 33];
+        encoded[0] = if self.y.is_odd().into() { 0x03 } else { 0x02 };
+        encoded[1..33].copy_from_slice(&self.x.to_bytes());
+
+        CompressedCurvePoint::from_bytes(GenericArray::clone_from_slice(&encoded[..]))
+            .expect("we encoded it correctly")
+    }
+
+    /// Returns the SEC-1 uncompressed encoding of this point.
+    pub fn to_uncompressed_pubkey(&self) -> UncompressedCurvePoint<Secp256k1> {
+        let mut encoded = [0; 65];
+        encoded[0] = 0x04;
+        encoded[1..33].copy_from_slice(&self.x.to_bytes());
+        encoded[33..65].copy_from_slice(&self.y.to_bytes());
+
+        UncompressedCurvePoint::from_bytes(GenericArray::clone_from_slice(&encoded[..]))
+            .expect("we encoded it correctly")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{AffinePoint, CURVE_EQUATION_B};
+    use crate::PublicKey;
+
+    const CURVE_EQUATION_B_BYTES: &str =
+        "0000000000000000000000000000000000000000000000000000000000000007";
+
+    const UNCOMPRESSED_BASEPOINT: &str =
+        "0479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8";
+    const COMPRESSED_BASEPOINT: &str =
+        "0279BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798";
+
+    #[test]
+    fn verify_constants() {
+        assert_eq!(
+            hex::encode(CURVE_EQUATION_B.to_bytes()).to_uppercase(),
+            CURVE_EQUATION_B_BYTES
+        );
+    }
+
+    #[test]
+    fn uncompressed_round_trip() {
+        let pubkey = PublicKey::from_bytes(&hex::decode(UNCOMPRESSED_BASEPOINT).unwrap()).unwrap();
+        let res: PublicKey = AffinePoint::from_pubkey(&pubkey)
+            .unwrap()
+            .to_uncompressed_pubkey()
+            .into();
+
+        assert_eq!(res, pubkey);
+    }
+
+    #[test]
+    fn compressed_round_trip() {
+        let pubkey = PublicKey::from_bytes(&hex::decode(COMPRESSED_BASEPOINT).unwrap()).unwrap();
+        let res: PublicKey = AffinePoint::from_pubkey(&pubkey)
+            .unwrap()
+            .to_compressed_pubkey()
+            .into();
+
+        assert_eq!(res, pubkey);
+    }
+
+    #[test]
+    fn uncompressed_to_compressed() {
+        let encoded = PublicKey::from_bytes(&hex::decode(UNCOMPRESSED_BASEPOINT).unwrap()).unwrap();
+
+        let res = AffinePoint::from_pubkey(&encoded)
+            .unwrap()
+            .to_compressed_pubkey();
+
+        assert_eq!(
+            hex::encode(res.as_bytes()).to_uppercase(),
+            COMPRESSED_BASEPOINT
+        );
+    }
+
+    #[test]
+    fn compressed_to_uncompressed() {
+        let encoded = PublicKey::from_bytes(&hex::decode(COMPRESSED_BASEPOINT).unwrap()).unwrap();
+
+        let res = AffinePoint::from_pubkey(&encoded)
+            .unwrap()
+            .to_uncompressed_pubkey();
+
+        assert_eq!(
+            hex::encode(res.as_bytes()).to_uppercase(),
+            UNCOMPRESSED_BASEPOINT
+        );
+    }
+}
---
+++ k256/src/arithmetic/util.rs
@@ -0,0 +1,29 @@
+//! Helper functions.
+
+/// Computes a + b + carry, returning the result along with the new carry.
+#[inline(always)]
+pub const fn adc(a: u64, b: u64, carry: u64) -> (u64, u64) {
+    let ret = (a as u128) + (b as u128) + (carry as u128);
+    (ret as u64, (ret >> 64) as u64)
+}
+
+/// Computes a - (b + borrow), returning the result along with the new borrow.
+#[inline(always)]
+pub const fn sbb(a: u64, b: u64, borrow: u64) -> (u64, u64) {
+    let ret = (a as u128).wrapping_sub((b as u128) + ((borrow >> 63) as u128));
+    (ret as u64, (ret >> 64) as u64)
+}
+
+/// Computes a + (b * c) + carry, returning the result along with the new carry.
+#[inline(always)]
+pub const fn mac(a: u64, b: u64, c: u64, carry: u64) -> (u64, u64) {
+    let ret = (a as u128) + ((b as u128) * (c as u128)) + (carry as u128);
+    (ret as u64, (ret >> 64) as u64)
+}
+
+#[inline(always)]
+/// Computes a multiply and carry via a shift and subtraction for the max of a type.
+pub const fn mac_typemax(a: u64, b: u64, carry: u64) -> (u64, u64) {
+    let ret = (a as u128) + (((b as u128) << 64) - (b as u128)) + (carry as u128);
+    (ret as u64, (ret >> 64) as u64)
+}
