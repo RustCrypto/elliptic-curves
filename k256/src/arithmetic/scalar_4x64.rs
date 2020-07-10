36170fb7c770ee1cc7cb363b9c5755266124b452
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-07-09 22:36:51 -0700

    rustfmt run

--- k256/src/arithmetic/scalar_4x64.rs
+++ k256/src/arithmetic/scalar_4x64.rs
@@ -69,7 +69,6 @@ fn adc_array_with_overflow(lhs: &[u64; 4], rhs: &[u64; 4]) -> ([u64; 4], Choice)
     (res, Choice::from(carry as u8))
 }
 
-
 #[inline(always)]
 fn conditional_select(a: &[u64; 4], b: &[u64; 4], choice: Choice) -> [u64; 4] {
     [
@@ -94,16 +93,14 @@ fn sumadd(a: u64, c0: u64, c1: u64, c2: u64) -> (u64, u64, u64) {
     let new_c0 = c0.wrapping_add(a); // overflow is handled on the next line
     let over = ct_less(new_c0, a);
     let new_c1 = c1.wrapping_add(over); // overflow is handled on the next line
-    // never overflows by contract
-    let new_c2 = c2 + ct_less(new_c1, over);
+    let new_c2 = c2 + ct_less(new_c1, over); // never overflows by contract
     (new_c0, new_c1, new_c2)
 }
 
 /// Add a to the number defined by (c0,c1). c1 must never overflow, c2 must be zero.
 fn sumadd_fast(a: u64, c0: u64, c1: u64) -> (u64, u64) {
     let new_c0 = c0.wrapping_add(a); // overflow is handled on the next line
-    // never overflows by contract (verified the next line)
-    let new_c1 = c1 + ct_less(new_c0, a);
+    let new_c1 = c1 + ct_less(new_c0, a); // never overflows by contract (verified the next line)
     debug_assert!((new_c1 != 0) | (new_c0 >= a));
     (new_c0, new_c1)
 }
@@ -117,8 +114,7 @@ fn muladd(a: u64, b: u64, c0: u64, c1: u64, c2: u64) -> (u64, u64, u64) {
     let new_c0 = c0.wrapping_add(tl); // overflow is handled on the next line
     let new_th = th + if new_c0 < tl { 1 } else { 0 }; // at most 0xFFFFFFFFFFFFFFFF
     let new_c1 = c1.wrapping_add(new_th); // overflow is handled on the next line
-    // never overflows by contract (verified in the next line)
-    let new_c2 = c2 + ct_less(new_c1, new_th);
+    let new_c2 = c2 + ct_less(new_c1, new_th); // never overflows by contract (verified in the next line)
     debug_assert!((new_c1 >= new_th) || (new_c2 != 0));
     (new_c0, new_c1, new_c2)
 }
