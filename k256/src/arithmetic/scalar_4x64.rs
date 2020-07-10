df965a890ccc8a2a9da6120788ac8a7f4365efdc
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-07-09 22:35:01 -0700

    Get rid of conditionals in sumadd/muladd

--- k256/src/arithmetic/scalar_4x64.rs
+++ k256/src/arithmetic/scalar_4x64.rs
@@ -84,47 +84,54 @@ fn conditional_select(a: &[u64; 4], b: &[u64; 4], choice: Choice) -> [u64; 4] {
 #[cfg_attr(docsrs, doc(cfg(feature = "arithmetic")))]
 pub struct Scalar4x64([u64; LIMBS]);
 
-/** Add a to the number defined by (c0,c1,c2). c2 must never overflow. */
+#[inline(always)]
+fn ct_less(a: u64, b: u64) -> u64 {
+    (a < b) as u64
+}
+
+/// Add a to the number defined by (c0,c1,c2). c2 must never overflow.
 fn sumadd(a: u64, c0: u64, c1: u64, c2: u64) -> (u64, u64, u64) {
-    let new_c0 = c0.wrapping_add(a); /* overflow is handled on the next line */
-    let over: u64 = if new_c0 < a { 1 } else { 0 };
-    let new_c1 = c1.wrapping_add(over); /* overflow is handled on the next line */
-    let new_c2 = c2 + if new_c1 < over { 1 } else { 0 }; /* never overflows by contract */
+    let new_c0 = c0.wrapping_add(a); // overflow is handled on the next line
+    let over = ct_less(new_c0, a);
+    let new_c1 = c1.wrapping_add(over); // overflow is handled on the next line
+    // never overflows by contract
+    let new_c2 = c2 + ct_less(new_c1, over);
     (new_c0, new_c1, new_c2)
 }
 
-/** Add a to the number defined by (c0,c1). c1 must never overflow, c2 must be zero. */
+/// Add a to the number defined by (c0,c1). c1 must never overflow, c2 must be zero.
 fn sumadd_fast(a: u64, c0: u64, c1: u64) -> (u64, u64) {
-    let new_c0 = c0.wrapping_add(a); /* overflow is handled on the next line */
-    let new_c1 = c1 + if new_c0 < a { 1 } else { 0 }; /* never overflows by contract (verified the next line) */
+    let new_c0 = c0.wrapping_add(a); // overflow is handled on the next line
+    // never overflows by contract (verified the next line)
+    let new_c1 = c1 + ct_less(new_c0, a);
     debug_assert!((new_c1 != 0) | (new_c0 >= a));
     (new_c0, new_c1)
 }
 
-/** Add a*b to the number defined by (c0,c1,c2). c2 must never overflow. */
+/// Add a*b to the number defined by (c0,c1,c2). c2 must never overflow.
 fn muladd(a: u64, b: u64, c0: u64, c1: u64, c2: u64) -> (u64, u64, u64) {
     let t = (a as u128) * (b as u128);
-    let th = (t >> 64) as u64; /* at most 0xFFFFFFFFFFFFFFFE */
+    let th = (t >> 64) as u64; // at most 0xFFFFFFFFFFFFFFFE
     let tl = t as u64;
 
-    let new_c0 = c0.wrapping_add(tl); /* overflow is handled on the next line */
-    let new_th = th + if new_c0 < tl { 1 } else { 0 }; /* at most 0xFFFFFFFFFFFFFFFF */
-    let new_c1 = c1.wrapping_add(new_th); /* overflow is handled on the next line */
-    let new_c2 = c2 + if new_c1 < new_th { 1 } else { 0 }; /* never overflows by contract (verified in the next line) */
+    let new_c0 = c0.wrapping_add(tl); // overflow is handled on the next line
+    let new_th = th + if new_c0 < tl { 1 } else { 0 }; // at most 0xFFFFFFFFFFFFFFFF
+    let new_c1 = c1.wrapping_add(new_th); // overflow is handled on the next line
+    // never overflows by contract (verified in the next line)
+    let new_c2 = c2 + ct_less(new_c1, new_th);
     debug_assert!((new_c1 >= new_th) || (new_c2 != 0));
     (new_c0, new_c1, new_c2)
 }
 
-/** Add a*b to the number defined by (c0,c1). c1 must never overflow. */
+/// Add a*b to the number defined by (c0,c1). c1 must never overflow.
 fn muladd_fast(a: u64, b: u64, c0: u64, c1: u64) -> (u64, u64) {
     let t = (a as u128) * (b as u128);
-    let th = (t >> 64) as u64; /* at most 0xFFFFFFFFFFFFFFFE */
+    let th = (t >> 64) as u64; // at most 0xFFFFFFFFFFFFFFFE
     let tl = t as u64;
 
-    let new_c0 = c0.wrapping_add(tl); /* overflow is handled on the next line */
-    // FIXME: constant time
-    let new_th = th + if new_c0 < tl { 1 } else { 0 }; /* at most 0xFFFFFFFFFFFFFFFF */
-    let new_c1 = c1 + new_th; /* never overflows by contract (verified in the next line) */
+    let new_c0 = c0.wrapping_add(tl); // overflow is handled on the next line
+    let new_th = th + ct_less(new_c0, tl); // at most 0xFFFFFFFFFFFFFFFF
+    let new_c1 = c1 + new_th; // never overflows by contract (verified in the next line)
     debug_assert!(new_c1 >= new_th);
     (new_c0, new_c1)
 }
