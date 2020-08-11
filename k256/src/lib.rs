0bb9395bf77a3405ac30c8dda3820167fa92aa91
Tony Arcieri <bascule@gmail.com>
2020-08-10 22:37:17 -0700

    k256 v0.4.1 (#132)

--- k256/src/lib.rs
+++ k256/src/lib.rs
@@ -39,7 +39,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/k256/0.4.0"
+    html_root_url = "https://docs.rs/k256/0.4.1"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
