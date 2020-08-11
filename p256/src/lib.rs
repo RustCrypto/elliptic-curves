4fc3e372c15f9bec2f95104992b2697ed9c9067f
Tony Arcieri <bascule@gmail.com>
2020-08-10 20:51:38 -0700

    p256 v0.4.0 (#129)

--- p256/src/lib.rs
+++ p256/src/lib.rs
@@ -32,7 +32,11 @@
 
 #![no_std]
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![doc(html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_root_url = "https://docs.rs/p256/0.4.0"
+)]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
 
