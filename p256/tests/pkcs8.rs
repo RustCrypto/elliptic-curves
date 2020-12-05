eddede278179b3523ce5d67b1d630ea9b712dcfa
Tony Arcieri <bascule@gmail.com>
2020-12-05 10:50:05 -0800

    p256: test `pkcs8::FromPublicKey` impl on `elliptic_curve::PublicKey` (#245)
    Adds tests which parse an example SPKI public key for NIST P-256, which
    exercise the corresponding support in the `elliptic_curve` crate:
    
    https://github.com/RustCrypto/traits/pull/385

--- p256/tests/pkcs8.rs
+++ p256/tests/pkcs8.rs
@@ -3,28 +3,58 @@
 #![cfg(feature = "pkcs8")]
 
 use hex_literal::hex;
-use p256::pkcs8::FromPkcs8;
+use p256::{
+    elliptic_curve::sec1::ToEncodedPoint,
+    pkcs8::{FromPrivateKey, FromPublicKey},
+};
 
 /// DER-encoded PKCS#8 private key
-const PKCS8_DER: &[u8; 138] = include_bytes!("examples/pkcs8-private-key.der");
+const PKCS8_PRIVATE_KEY_DER: &[u8; 138] = include_bytes!("examples/pkcs8-private-key.der");
+
+/// DER-encoded PKCS#8 public key
+const PKCS8_PUBLIC_KEY_DER: &[u8; 91] = include_bytes!("examples/pkcs8-public-key.der");
 
 /// PEM-encoded PKCS#8 private key
 #[cfg(feature = "pem")]
-const PKCS8_PEM: &str = include_str!("examples/pkcs8-private-key.pem");
+const PKCS8_PRIVATE_KEY_PEM: &str = include_str!("examples/pkcs8-private-key.pem");
+
+/// PEM-encoded PKCS#8 public key
+#[cfg(feature = "pem")]
+const PKCS8_PUBLIC_KEY_PEM: &str = include_str!("examples/pkcs8-public-key.pem");
 
 #[test]
 fn parse_pkcs8_private_key_from_der() {
-    let secret_key = p256::SecretKey::from_pkcs8_der(&PKCS8_DER[..]).unwrap();
+    let secret_key = p256::SecretKey::from_pkcs8_der(&PKCS8_PRIVATE_KEY_DER[..]).unwrap();
     let expected_scalar = hex!("69624171561A63340DE0E7D869F2A05492558E1A04868B6A9F854A866788188D");
     assert_eq!(secret_key.to_bytes().as_slice(), &expected_scalar[..]);
 }
 
 #[test]
+fn parse_pkcs8_public_key_from_der() {
+    let public_key = p256::PublicKey::from_public_key_der(&PKCS8_PUBLIC_KEY_DER[..]).unwrap();
+    let expected_sec1_point = hex!("041CACFFB55F2F2CEFD89D89EB374B2681152452802DEEA09916068137D839CF7FC481A44492304D7EF66AC117BEFE83A8D08F155F2B52F9F618DD447029048E0F");
+    assert_eq!(
+        public_key.to_encoded_point(false).as_bytes(),
+        &expected_sec1_point[..]
+    );
+}
+
+#[test]
 #[cfg(feature = "pem")]
 fn parse_pkcs8_private_key_from_pem() {
-    let secret_key = PKCS8_PEM.parse::<p256::SecretKey>().unwrap();
+    let secret_key = PKCS8_PRIVATE_KEY_PEM.parse::<p256::SecretKey>().unwrap();
 
     // Ensure key parses equivalently to DER
-    let der_key = p256::SecretKey::from_pkcs8_der(&PKCS8_DER[..]).unwrap();
+    let der_key = p256::SecretKey::from_pkcs8_der(&PKCS8_PRIVATE_KEY_DER[..]).unwrap();
     assert_eq!(secret_key.to_bytes(), der_key.to_bytes());
+}
+
+#[test]
+#[cfg(feature = "pem")]
+fn parse_pkcs8_public_key_from_pem() {
+    let public_key = PKCS8_PUBLIC_KEY_PEM.parse::<p256::PublicKey>().unwrap();
+
+    // Ensure key parses equivalently to DER
+    let der_key = p256::PublicKey::from_public_key_der(&PKCS8_PUBLIC_KEY_DER[..]).unwrap();
+    assert_eq!(public_key, der_key);
 }
