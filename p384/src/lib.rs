ddffc54e35d30ff38f0e15834cc1f02244b28d40
Tony Arcieri <bascule@gmail.com>
2020-12-06 11:22:14 -0800

    p384 v0.5.0 (#255)

--- p384/src/lib.rs
+++ p384/src/lib.rs
@@ -12,7 +12,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/p384/0.4.1"
+    html_root_url = "https://docs.rs/p384/0.5.0"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
