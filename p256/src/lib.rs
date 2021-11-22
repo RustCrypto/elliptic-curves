12686972ebb0d977e0db461fb5dde3c3fa96f1d5
Tony Arcieri <bascule@gmail.com>
2021-11-22 09:12:59 -0700

    p256 v0.10.0-pre.1 (#469)

--- p256/src/lib.rs
+++ p256/src/lib.rs
@@ -44,7 +44,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/p256/0.10.0-pre"
+    html_root_url = "https://docs.rs/p256/0.10.0-pre.1"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
