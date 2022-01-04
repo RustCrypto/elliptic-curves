b5ed9e03ad90264c25e564f3940512c5f518995b
Bogdan Opanchuk <bogdan@opanchuk.net>
2022-01-03 22:08:50 -0800

    Make `order_minus_one` a constant

--- k256/src/arithmetic/scalar.rs
+++ k256/src/arithmetic/scalar.rs
@@ -591,8 +591,8 @@ impl Reduce<U512> for Scalar {
 
 impl ReduceNonZero<U256> for Scalar {
     fn from_uint_reduced_nonzero(w: U256) -> Self {
-        let order_minus_one = ORDER.wrapping_sub(&U256::ONE);
-        let (r, underflow) = w.sbb(&order_minus_one, Limb::ZERO);
+        const ORDER_MINUS_ONE: U256 = ORDER.wrapping_sub(&U256::ONE);
+        let (r, underflow) = w.sbb(&ORDER_MINUS_ONE, Limb::ZERO);
         let underflow = Choice::from((underflow.0 >> (Limb::BIT_SIZE - 1)) as u8);
         Self(U256::conditional_select(&w, &r, !underflow).wrapping_add(&U256::ONE))
     }
