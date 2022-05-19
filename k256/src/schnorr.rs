c0658b403b6a70c1de086683cd6b8e086f65f1dd
Nicolas Stalder <nstalder@jumpcrypto.com>
2022-05-19 08:57:49 +0000

    Add BIP340 verify vectors; all but index 9 pass

--- k256/src/schnorr.rs
+++ k256/src/schnorr.rs
@@ -12,7 +12,7 @@ use elliptic_curve::{
     bigint::U256,
     ops::{LinearCombination, Reduce},
     subtle::ConditionallySelectable,
-    // DecompactPoint,
+    DecompactPoint,
 };
 use sha2::{Digest, Sha256};
 use signature::{Error, Result};
@@ -26,10 +26,29 @@ const CHALLENGE_TAG: &[u8] = b"BIP0340/challenge";
 /// [BIP340]: https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 pub struct Signature {
+    // TODO(nickray): maybe store (FieldElement, NonScalar), avoiding the unwraps.
+    // However, this
+    // a) contradicts tarcieri's philosophy of storing "bag of bytes"
+    // b) makes the Debug, Eq, PartialEq, PartialOrd, Ord impls tricky,
+    // as FieldElement + NonZeroScalar don't implement them, so we'd have to
+    // either modify upstream or implement in terms of the calculated `to_bytes`.
     bytes: [u8; 64],
 }
 
 impl Signature {
+    /// Parse signature from big endian-encoded bytes.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        let bytes: [u8; 64] = bytes.try_into()
+            .map_err(|_| Error::new())?;
+
+        let _: FieldElement = Option::from(FieldElement::from_bytes(FieldBytes::from_slice(&bytes[..32])))
+            .ok_or(Error::new())?;
+        let _: NonZeroScalar = Option::from(NonZeroScalar::from_repr(*FieldBytes::from_slice(&bytes[32..])))
+            .ok_or(Error::new())?;
+
+        Ok(Self { bytes })
+    }
+
     /// Borrow the serialized signature as bytes.
     pub fn as_bytes(&self) -> &[u8; 64] {
         &self.bytes
@@ -204,19 +223,16 @@ impl VerifyingKey {
         self.as_affine().x.to_bytes()
     }
 
-    // pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
-    //     // let bytes: FieldBytes = bytes
-    //     //     .try_into()
-    //     //     .map_err(|_| Error::new())?;
-    //
-    //     let maybe_affine_point = AffinePoint::decompact(FieldBytes::from_slice(bytes));
-    //     let affine_point = match maybe_affine_point.is_some().into() {
-    //         true => maybe_affine_point.unwrap(),
-    //         false => return Err(Error::new()),
-    //     };
-    //
-    //     Ok(Self { inner: PublicKey::from_affine(affine_point).map_err(|_| Error::new())? })
-    // }
+    /// Parse verifying key from big endian-encoded x-coordinate.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        let maybe_affine_point = AffinePoint::decompact(FieldBytes::from_slice(bytes));
+        let affine_point = match maybe_affine_point.is_some().into() {
+            true => maybe_affine_point.unwrap(),
+            false => return Err(Error::new()),
+        };
+
+        Ok(Self { inner: PublicKey::from_affine(affine_point).map_err(|_| Error::new())? })
+    }
 }
 
 impl From<VerifyingKey> for AffinePoint {
@@ -250,14 +266,12 @@ fn xor(a: &FieldBytes, b: &FieldBytes) -> FieldBytes {
     res
 }
 
-// TODO(tarcieri): verification tests
+// Test vectors from:
+// https://github.com/bitcoin/bips/blob/master/bip-0340/test-vectors.csv
 #[cfg(test)]
 mod tests {
-    use super::SigningKey;
+    use super::{Signature, SigningKey, VerifyingKey};
     use hex_literal::hex;
-
-    // Test vectors from:
-    // https://docs.rs/argon2/latest/argon2/struct.Argon2.html#method.new_with_secret
 
     /// Signing test vector
     struct SignVector {
@@ -313,7 +327,7 @@ mod tests {
             ),
         },
         // index 3
-        // TODO(tarcieri): failing; test vector notes: "test fails if msg is reduced modulo p or n"
+        // test fails if msg is reduced modulo p or n
         SignVector {
             secret_key: hex!("0B432B2677937381AEF05BB02A66ECD012773062CF3FA2549E44F58ED2401710"),
             public_key: hex!("25D1DFF95105F5253C4022F628A996AD3A0D95FBF21D468A1B33F8C160D8F517"),
@@ -339,4 +353,162 @@ mod tests {
             assert_eq!(&vector.signature, sig.as_ref());
         }
     }
+
+    /// Verification test vector
+    struct VerifyVector {
+        /// Index of test case
+        index: u8,
+
+        /// Verifying key
+        public_key: [u8; 32],
+
+        /// Message digest
+        message: [u8; 32],
+
+        /// Claimed signature
+        signature: [u8; 64],
+
+        /// Is signature valid
+        valid: bool,
+    }
+
+    /// BIP340 verification test vectors: index 4-14
+    const BIP340_VERIFY_VECTORS: &[VerifyVector] = &[
+        // index 4
+        VerifyVector {
+            index: 4,
+            public_key: hex!("D69C3509BB99E412E68B0FE8544E72837DFA30746D8BE2AA65975F29D22DC7B9"),
+            message: hex!("4DF3C3F68FCC83B27E9D42C90431A72499F17875C81A599B566C9889B9696703"),
+            signature: hex!(
+                "00000000000000000000003B78CE563F89A0ED9414F5AA28AD0D96D6795F9C63
+                 76AFB1548AF603B3EB45C9F8207DEE1060CB71C04E80F593060B07D28308D7F4"
+            ),
+            valid: true,
+        },
+        // index 5
+        // public key not on curve
+        VerifyVector {
+            index: 5,
+            public_key: hex!("EEFDEA4CDB677750A420FEE807EACF21EB9898AE79B9768766E4FAA04A2D4A34"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "6CFF5C3BA86C69EA4B7376F31A9BCB4F74C1976089B2D9963DA2E5543E177769
+                 69E89B4C5564D00349106B8497785DD7D1D713A8AE82B32FA79D5F7FC407D39B"),
+            valid: false,
+        },
+        // index 6
+        // has_even_y(R) is false
+        VerifyVector {
+            index: 6,
+            public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "FFF97BD5755EEEA420453A14355235D382F6472F8568A18B2F057A1460297556
+                 3CC27944640AC607CD107AE10923D9EF7A73C643E166BE5EBEAFA34B1AC553E2"),
+            valid: false,
+        },
+        // index 7
+        // negated message
+        VerifyVector {
+            index: 7,
+            public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "1FA62E331EDBC21C394792D2AB1100A7B432B013DF3F6FF4F99FCB33E0E1515F
+                 28890B3EDB6E7189B630448B515CE4F8622A954CFE545735AAEA5134FCCDB2BD"),
+            valid: false,
+        },
+        // index 8
+        // negated s value
+        VerifyVector {
+            index: 8,
+            public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "6CFF5C3BA86C69EA4B7376F31A9BCB4F74C1976089B2D9963DA2E5543E177769
+                 961764B3AA9B2FFCB6EF947B6887A226E8D7C93E00C5ED0C1834FF0D0C2E6DA6"),
+            valid: false,
+        },
+        // // index 9
+        // // sG - eP is infinite. Test fails in single verification if has_even_y(inf) is defined as true and x(inf) as 0
+        // VerifyVector {
+        //     index: 9,
+        //     public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+        //     message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+        //     signature: hex!(
+        //         "0000000000000000000000000000000000000000000000000000000000000000
+        //          123DDA8328AF9C23A94C1FEECFD123BA4FB73476F0D594DCB65C6425BD186051"),
+        //     valid: false,
+        // },
+        // index 10
+        // sG - eP is infinite. Test fails in single verification if has_even_y(inf) is defined as true and x(inf) as 1
+        VerifyVector {
+            index: 10,
+            public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "0000000000000000000000000000000000000000000000000000000000000001
+                 7615FBAF5AE28864013C099742DEADB4DBA87F11AC6754F93780D5A1837CF197"),
+            valid: false,
+        },
+        // index 11
+        // sig[0:32] is not an X coordinate on the curve
+        VerifyVector {
+            index: 11,
+            public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "4A298DACAE57395A15D0795DDBFD1DCB564DA82B0F269BC70A74F8220429BA1D
+                 69E89B4C5564D00349106B8497785DD7D1D713A8AE82B32FA79D5F7FC407D39B"),
+            valid: false,
+        },
+        // index 12
+        // sig[0:32] is equal to field size
+        VerifyVector {
+            index: 12,
+            public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+                 69E89B4C5564D00349106B8497785DD7D1D713A8AE82B32FA79D5F7FC407D39B"),
+            valid: false,
+        },
+        // index 13
+        // sig[32:64] is equal to curve order
+        VerifyVector {
+            index: 13,
+            public_key: hex!("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "6CFF5C3BA86C69EA4B7376F31A9BCB4F74C1976089B2D9963DA2E5543E177769
+                 FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141"),
+            valid: false,
+        },
+        // index 14
+        // public key is not a valid X coordinate because it exceeds the field size
+        VerifyVector {
+            index: 14,
+            public_key: hex!("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC30"),
+            message: hex!("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89"),
+            signature: hex!(
+                "6CFF5C3BA86C69EA4B7376F31A9BCB4F74C1976089B2D9963DA2E5543E177769
+                 69E89B4C5564D00349106B8497785DD7D1D713A8AE82B32FA79D5F7FC407D39B"),
+            valid: false,
+        },
+    ];
+
+    #[test]
+    fn bip340_verify_vectors() {
+        for vector in BIP340_VERIFY_VECTORS {
+            let maybe_pk = VerifyingKey::from_bytes(&vector.public_key);
+            let maybe_sig = Signature::from_bytes(&vector.signature);
+            let verification = match (maybe_pk, maybe_sig) {
+                (Ok(pk), Ok(sig)) => pk.verify_raw_digest(&vector.message, &sig).is_ok(),
+                _ => false,
+            };
+
+            assert_eq!(vector.valid, verification, "incorrect validation for index {}", vector.index);
+        }
+    }
+
 }
