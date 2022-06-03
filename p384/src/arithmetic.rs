0dbf9395a1ecd04cc705c0ae8816369be314d416
Tony Arcieri <bascule@gmail.com>
2022-06-03 10:40:23 -0600

    p384: use canonical form constants (#592)
    Now that conversions to/from the Montgomery domain are `const fn` (#590)
    it's possible to define constants in their canonical form, rather than
    converting them into the Montgomery domain first.
    
    This makes the code clearer and easier to audit.

--- p384/src/arithmetic.rs
+++ p384/src/arithmetic.rs
@@ -19,15 +19,14 @@ use self::{
     projective::ProjectivePoint,
     scalar::Scalar,
 };
-use crate::U384;
 
 /// a = -3 (0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000fffffffc)
-///
-/// NOTE: field element has been translated into the Montgomery domain.
-const CURVE_EQUATION_A: FieldElement = FieldElement(U384::from_be_hex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbfffffffc0000000000000003fffffffc"));
+const CURVE_EQUATION_A: FieldElement = FieldElement::ZERO
+    .sub(&FieldElement::ONE)
+    .sub(&FieldElement::ONE)
+    .sub(&FieldElement::ONE);
 
 /// b = b3312fa7 e23ee7e4 988e056b e3f82d19 181d9c6e fe814112
 ///     0314088f 5013875a c656398d 8a2ed19d 2a85c8ed d3ec2aef
-///
-/// NOTE: field element has been translated into the Montgomery domain.
-const CURVE_EQUATION_B: FieldElement = FieldElement(U384::from_be_hex("cd08114b604fbff9b62b21f41f022094e3374bee94938ae277f2209b1920022ef729add87a4c32ec081188719d412dcc"));
+const CURVE_EQUATION_B: FieldElement =
+    FieldElement::from_be_hex("b3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef");
