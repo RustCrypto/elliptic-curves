321c905495cda6d6ded44171112f21571153a9c9
Tony Arcieri <bascule@gmail.com>
2023-02-11 18:19:39 -0700

    p521: `FieldElement` with fiat-crypto arithmetic
    Adds a P-521 base field implementation generated by fiat-crypto, run with
    the following arguments:
    
        $ unsaturated_solinas --lang Rust --inline p521 64 9 '2^521 - 1' carry_mul carry_square carry add sub opp selectznz to_bytes from_bytes relax
    
    This leverages special properties of the prime 2^521 - 1, which is a
    Mersenne/Solinas prime. This makes it different from the other
    fiat-crypto field implementations in the project, which use Montgomery
    form internally.
    
    The wrappers for the field implementation are handwritten as opposed to
    using the `primeorder::impl_field_element!` macro. Additionally there's
    a `LooseFieldElement` type internally which can be used to defer
    reductions, similar to how `FieldElement` is implemented in `k256`.
    
    The output has been postprocessed by the `fiat-constify` utility, which
    rewrites the functions in the fiat-crypto output as `const fn`.
    
    For now there's only a 64-bit backend as `unsaturated_solinas` does not
    seem to support generating a 32-bit backend (and when I've tried with
    `word_by_word_montgomery` I get stack overflows). However, it's written
    so it should still compile on 32-bit platforms.
    
    Constants for the `PrimeField` impl were calculated in `sage` as
    follows:
    
    sage: p = 2^521 - 1
    sage: multiplicative_generator = GF(p).primitive_element()
    sage: p_minus_1_bin = (p - 1).binary()
    sage: s = len(p_minus_1_bin) - len(p_minus_1_bin.rstrip('0')) # count trailing zeros in binary
    sage: t = (p - 1) >> s
    sage: root_of_unity = pow(multiplicative_generator,t,p)
    sage: delta = multiplicative_generator ^ (2 ^ s)
    sage: multiplicative_generator
    3
    sage: p_minus_1_bin
    '11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111110'
    sage: s
    1
    sage: hex(t)
    '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
    sage: hex(root_of_unity)
    '0x1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe'
    sage: delta
    9

---
+++ p521/src/arithmetic/field/loose.rs
@@ -0,0 +1,77 @@
+use super::{field_impl::*, FieldElement};
+use core::ops::Mul;
+
+/// "Loose" field element.
+pub(crate) struct LooseFieldElement(pub(super) fiat_p521_loose_field_element);
+
+impl LooseFieldElement {
+    /// Reduce field element.
+    pub(crate) const fn carry(&self) -> FieldElement {
+        FieldElement(fiat_p521_carry(&self.0))
+    }
+
+    /// Multiplies two field elements and reduces the result.
+    pub(crate) const fn carry_mul(&self, rhs: &Self) -> FieldElement {
+        FieldElement(fiat_p521_carry_mul(&self.0, &rhs.0))
+    }
+
+    /// Squares a field element and reduces the result.
+    pub(crate) const fn carry_square(&self) -> FieldElement {
+        FieldElement(fiat_p521_carry_square(&self.0))
+    }
+}
+
+impl From<FieldElement> for LooseFieldElement {
+    #[inline]
+    fn from(tight: FieldElement) -> LooseFieldElement {
+        LooseFieldElement::from(&tight)
+    }
+}
+
+impl From<&FieldElement> for LooseFieldElement {
+    #[inline]
+    fn from(tight: &FieldElement) -> LooseFieldElement {
+        tight.relax()
+    }
+}
+
+impl From<LooseFieldElement> for FieldElement {
+    #[inline]
+    fn from(loose: LooseFieldElement) -> FieldElement {
+        FieldElement::from(&loose)
+    }
+}
+
+impl From<&LooseFieldElement> for FieldElement {
+    #[inline]
+    fn from(loose: &LooseFieldElement) -> FieldElement {
+        loose.carry()
+    }
+}
+
+impl Mul for LooseFieldElement {
+    type Output = FieldElement;
+
+    #[inline]
+    fn mul(self, rhs: LooseFieldElement) -> FieldElement {
+        self.carry_mul(&rhs)
+    }
+}
+
+impl Mul<&LooseFieldElement> for LooseFieldElement {
+    type Output = FieldElement;
+
+    #[inline]
+    fn mul(self, rhs: &LooseFieldElement) -> FieldElement {
+        self.carry_mul(rhs)
+    }
+}
+
+impl Mul<&LooseFieldElement> for &LooseFieldElement {
+    type Output = FieldElement;
+
+    #[inline]
+    fn mul(self, rhs: &LooseFieldElement) -> FieldElement {
+        self.carry_mul(rhs)
+    }
+}
