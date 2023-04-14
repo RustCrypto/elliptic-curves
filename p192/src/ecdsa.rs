d6380b3723491abf118b457dbf47018612dd13ae
Halvor Lyche Strandvoll <halvors@halvors.org>
2023-04-14 16:37:52 +0200

    Remove tests for ECDSA signing in P-192 crate since it will not be supported there

--- p192/src/ecdsa.rs
+++ p192/src/ecdsa.rs
@@ -6,13 +6,11 @@
 //! - `ecdsa-core`: provides only the [`Signature`] type (which represents an
 //!   ECDSA/P-192 signature). Does not require the `arithmetic` feature. This is
 //!   useful for 3rd-party crates which wish to use the `Signature` type for
-//!   interoperability purposes (particularly in conjunction with the
-//!   [`signature::Signer`] trait. Example use cases for this include other
+//!   interoperability purposes. Example use cases for this include other
 //!   software implementations of ECDSA/P-192 and wrappers for cloud KMS
 //!   services or hardware devices (HSM or crypto hardware wallet).
-//! - `ecdsa`: provides `ecdsa-core` features plus the [`SigningKey`] and
-//!   [`VerifyingKey`] types which natively implement ECDSA/P-192 signing and
-//!   verification.
+//! - `ecdsa`: provides `ecdsa-core` features plus [`VerifyingKey`] types 
+//!   which natively implement ECDSA/P-192 verification.
 
 pub use ecdsa_core::signature::{self, Error};
 #[cfg(feature = "ecdsa")]
@@ -26,10 +24,6 @@ pub type Signature = ecdsa_core::Signature<NistP192>;
 /// ECDSA/P-192 signature (ASN.1 DER encoded)
 pub type DerSignature = ecdsa_core::der::Signature<NistP192>;
 
-/// ECDSA/P-192 signing key
-#[cfg(feature = "ecdsa")]
-pub type SigningKey = ecdsa_core::SigningKey<NistP192>;
-
 /// ECDSA/P-192 verification key (i.e. public key)
 #[cfg(feature = "ecdsa")]
 pub type VerifyingKey = ecdsa_core::VerifyingKey<NistP192>;
@@ -39,49 +33,8 @@ impl VerifyPrimitive<NistP192> for AffinePoint {}
 
 #[cfg(all(test, feature = "ecdsa"))]
 mod tests {
-    // use crate::ecdsa::{signature::hazmat::PrehashSigner, Signature, SigningKey};
-    // use hex_literal::hex;
-    // use sha1::{Digest, Sha1};
-
-    // Test vector from RFC 6979 Appendix 2.3 (NIST P-192 + SHA-1)
-    // <https://tools.ietf.org/html/rfc6979#appendix-A.2.3>
-    // #[test]
-    // fn rfc6979() {
-    //     let x = hex!("6FAB034934E4C0FC9AE67F5B5659A9D7D1FEFD187EE09FD4");
-    //     let signer = SigningKey::from_bytes(&x.into()).unwrap();
-
-    //     let signature: Signature = signer.sign_prehash(&Sha1::digest(b"sample"));
-    //     assert_eq!(
-    //         signature.to_bytes().as_slice(),
-    //         &hex!(
-    //             "98C6BD12B23EAF5E2A2045132086BE3EB8EBD62ABF6698FF
-    //              57A22B07DEA9530F8DE9471B1DC6624472E8E2844BC25B64"
-    //         )
-    //     );
-
-    //     let signature: Signature = signer.sign_prehash(&Sha1::digest(b"test"));
-    //     assert_eq!(
-    //         signature.to_bytes().as_slice(),
-    //         &hex!(
-    //             "0F2141A0EBBC44D2E1AF90A50EBCFCE5E197B3B7D4DE036D
-    //              EB18BC9E1F3D7387500CB99CF5F7C157070A8961E38700B7"
-    //         )
-    //     );
-    // }
-
-    // mod sign {
-
-    //     use crate::{test_vectors::ecdsa::ECDSA_TEST_VECTORS, NistP192};
-    //     ecdsa_core::new_signing_test!(NistP192, ECDSA_TEST_VECTORS);
-    // }
-
     mod verify {
         use crate::{test_vectors::ecdsa::ECDSA_TEST_VECTORS, NistP192};
         ecdsa_core::new_verification_test!(NistP192, ECDSA_TEST_VECTORS);
     }
-
-    // mod wycheproof {
-    //     use crate::NistP192;
-    //     ecdsa_core::new_wycheproof_test!(wycheproof, "wycheproof", NistP192);
-    // }
 }
