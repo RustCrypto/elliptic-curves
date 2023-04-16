07340d67088b373c148664a737514ce8a19df5e3
Tony Arcieri <bascule@gmail.com>
2023-04-16 09:37:24 -0600

    bp384: initial field elements with fiat-crypto arithmetic
    Adds `FieldElement` and `Scalar` field implementations generated by
    fiat-crypto, run with the following arguments:
    
        $ ./word_by_word_montgomery --lang Rust --inline bp384 32 0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b412b1da197fb71123acd3a729901d1a71874700133107ec53 mul square add sub opp from_montgomery to_montgomery nonzero selectznz to_bytes from_bytes one msat divstep divstep_precomp
        $ ./word_by_word_montgomery --lang Rust --inline bp384 64 0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b412b1da197fb71123acd3a729901d1a71874700133107ec53 mul square add sub opp from_montgomery to_montgomery nonzero selectznz to_bytes from_bytes one msat divstep divstep_precomp
    
        $ ./word_by_word_montgomery --lang Rust --inline bp384_scalar 32 0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b31f166e6cac0425a7cf3ab6af6b7fc3103b883202e9046565 mul square add sub opp from_montgomery to_montgomery nonzero selectznz to_bytes from_bytes one msat divstep divstep_precomp
        $ ./word_by_word_montgomery --lang Rust --inline bp384_scalar 64 0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b31f166e6cac0425a7cf3ab6af6b7fc3103b883202e9046565 mul square add sub opp from_montgomery to_montgomery nonzero selectznz to_bytes from_bytes one msat divstep divstep_precomp
    
    The implementation is currently gated on the `wip-arithmetic-do-not-use`
    crate feature.
    
    The output has been postprocessed by the `fiat-constify` utility, which
    rewrites the functions in the fiat-crypto output as `const fn`.

--- bp384/src/lib.rs
+++ bp384/src/lib.rs
@@ -18,8 +18,19 @@
 pub mod r1;
 pub mod t1;
 
+#[cfg(feature = "wip-arithmetic-do-not-use")]
+mod arithmetic;
+
 pub use crate::{r1::BrainpoolP384r1, t1::BrainpoolP384t1};
 pub use elliptic_curve::{self, bigint::U384};
 
 #[cfg(feature = "pkcs8")]
 pub use elliptic_curve::pkcs8;
+
+use elliptic_curve::generic_array::{typenum::U48, GenericArray};
+
+/// Byte representation of a base/scalar field element of a given curve.
+pub type FieldBytes = GenericArray<u8, U48>;
+
+const ORDER_HEX: &str = "8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b31f166e6cac0425a7cf3ab6af6b7fc3103b883202e9046565";
+const ORDER: U384 = U384::from_be_hex(ORDER_HEX);
