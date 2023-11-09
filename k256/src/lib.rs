a5e04223ec5ee7598de80cd1a9e5970c5f6254d2
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-11-09 02:02:22 +0200

    moved use

--- k256/src/lib.rs
+++ k256/src/lib.rs
@@ -28,8 +28,6 @@
 
 #[cfg(feature = "arithmetic")]
 mod arithmetic;
-#[cfg(feature = "arithmetic")]
-pub use arithmetic::lincomb;
 
 #[cfg(feature = "ecdh")]
 pub mod ecdh;
@@ -46,7 +44,7 @@ pub mod test_vectors;
 pub use elliptic_curve::{self, bigint::U256};
 
 #[cfg(feature = "arithmetic")]
-pub use arithmetic::{affine::AffinePoint, projective::ProjectivePoint, scalar::Scalar};
+pub use arithmetic::{affine::AffinePoint, lincomb, projective::ProjectivePoint, scalar::Scalar};
 
 #[cfg(feature = "expose-field")]
 pub use arithmetic::FieldElement;
