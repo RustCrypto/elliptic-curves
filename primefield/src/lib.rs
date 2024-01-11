0f29241264789919688f1284f46fffd1f1ef593e
Tony Arcieri <bascule@gmail.com>
2024-01-11 16:59:05 -0700

    primefield: crate boilerplate (#1013)
    Right now the `primeorder` crate, which contains elliptic curve addition
    formulas, is also curiously overloaded with some macros for generating
    field implementations, even though those have nothing in particular to
    do with the curve formulas.
    
    This is largely because `primeorder` was an existing dependency of the
    other crates, so it was a convenient place to put it.
    
    This begins the process of extracting it out into a separate crate. It
    should also make it easier to perform the `fiat-crypto` upgrade, since
    we can put the new implementation in `primefield` and leave the existing
    one in `primeorder` until all crates have been migrated.

---
+++ primefield/src/lib.rs
@@ -0,0 +1,9 @@
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+)]
+#![forbid(unsafe_code)]
+#![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
+#![doc = include_str!("../README.md")]
