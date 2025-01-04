76b2d214aeb1b4e42ca46ff186af8844aba72e43
Marco <wudmytrotest200@gmail.com>
2025-01-04 15:41:16 +0100

    typo ecdsa.rs

--- p192/src/test_vectors/ecdsa.rs
+++ p192/src/test_vectors/ecdsa.rs
@@ -9,7 +9,7 @@ use hex_literal::hex;
 /// (P-192, SHA-1, from `SigGen.txt` in `186-2ecdsatestvectors.zip`)
 /// <https://csrc.nist.gov/projects/cryptographic-algorithm-validation-program/digital-signatures>
 ///
-/// The `m` field contains a SHA-1 prehash of the `Msg` field in the
+/// The `m` field contains an SHA-1 prehash of the `Msg` field in the
 /// original `SigTen.txt`.
 
 pub const ECDSA_TEST_VECTORS: &[TestVector; 15] = &[
