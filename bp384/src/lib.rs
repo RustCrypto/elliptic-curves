9bc130a585318d79f3000850ae0fdd44e85bb866
Tony Arcieri <bascule@gmail.com>
2025-05-01 18:05:41 -0600

    bp384: upgrade to `fiat-crypto` v0.1.4
    Updates the field element type generating macros in the `primefield`
    crate to be compatible with the latest `fiat-crypto` code as
    postprocessed by `fiat-constify`, and regenerates the `bp384` field
    impls using `fiat-crypto` v0.1.4.

--- bp384/src/lib.rs
+++ bp384/src/lib.rs
@@ -22,7 +22,10 @@ pub mod t1;
 mod arithmetic;
 
 pub use crate::{r1::BrainpoolP384r1, t1::BrainpoolP384t1};
-pub use elliptic_curve::{self, bigint::U384};
+pub use elliptic_curve::{
+    self,
+    bigint::{ArrayEncoding, U384},
+};
 
 #[cfg(feature = "wip-arithmetic-do-not-use")]
 pub use crate::arithmetic::scalar::Scalar;
@@ -30,13 +33,20 @@ pub use crate::arithmetic::scalar::Scalar;
 #[cfg(feature = "pkcs8")]
 pub use elliptic_curve::pkcs8;
 
-use elliptic_curve::array::{Array, typenum::U48};
-
 #[cfg(feature = "wip-arithmetic-do-not-use")]
 pub(crate) use crate::arithmetic::field::FieldElement;
+use elliptic_curve::array::{Array, typenum::U48};
 
 /// Byte representation of a base/scalar field element of a given curve.
 pub type FieldBytes = Array<u8, U48>;
 
 const ORDER_HEX: &str = "8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b31f166e6cac0425a7cf3ab6af6b7fc3103b883202e9046565";
 const ORDER: U384 = U384::from_be_hex(ORDER_HEX);
+
+fn decode_field_bytes(field_bytes: &FieldBytes) -> U384 {
+    U384::from_be_byte_array(*field_bytes)
+}
+
+fn encode_field_bytes(uint: &U384) -> FieldBytes {
+    uint.to_be_byte_array()
+}
