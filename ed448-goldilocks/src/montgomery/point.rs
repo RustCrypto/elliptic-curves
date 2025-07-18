266feadb1ea820bab24acfb6c15ec9c2e50e0ba8
daxpedda <daxpedda@gmail.com>
2025-07-19 01:47:26 +0200

    Implement `CurveArithmetic` for Curve448

--- ed448-goldilocks/src/montgomery/point.rs
+++ ed448-goldilocks/src/montgomery/point.rs
@@ -1,11 +1,21 @@
-use elliptic_curve::bigint::U448;
-use subtle::Choice;
-use subtle::ConditionallySelectable;
-use subtle::ConstantTimeEq;
+use elliptic_curve::{
+    CurveGroup, Error, Group,
+    array::Array,
+    bigint::U448,
+    consts::U57,
+    group::GroupEncoding,
+    group::cofactor::CofactorGroup,
+    group::prime::PrimeGroup,
+    ops::LinearCombination,
+    point::{AffineCoordinates, NonIdentity},
+    zeroize::DefaultIsZeroes,
+};
+use rand_core::TryRngCore;
+use subtle::{Choice, ConditionallySelectable, ConstantTimeEq, CtOption};
 
-use super::{MontgomeryXpoint, ProjectiveMontgomeryXpoint};
-use crate::AffinePoint;
+use super::{MontgomeryScalar, MontgomeryXpoint, ProjectiveMontgomeryXpoint};
 use crate::field::{ConstMontyType, FieldElement};
+use crate::{AffinePoint, Curve448FieldBytes, ORDER};
 
 /// A point in Montgomery form including the y-coordinate.
 #[derive(Copy, Clone, Debug, Default, Eq)]
@@ -116,6 +126,43 @@ impl From<&MontgomeryPoint> for AffinePoint {
 impl From<MontgomeryPoint> for AffinePoint {
     fn from(value: MontgomeryPoint) -> Self {
         (&value).into()
+    }
+}
+
+impl DefaultIsZeroes for MontgomeryPoint {}
+
+impl AffineCoordinates for MontgomeryPoint {
+    type FieldRepr = Curve448FieldBytes;
+
+    fn x(&self) -> Self::FieldRepr {
+        self.x.to_bytes().into()
+    }
+
+    fn y(&self) -> Self::FieldRepr {
+        self.y.to_bytes().into()
+    }
+
+    fn x_is_odd(&self) -> Choice {
+        self.x.is_negative()
+    }
+
+    fn y_is_odd(&self) -> Choice {
+        self.y.is_negative()
+    }
+}
+
+impl From<NonIdentity<MontgomeryPoint>> for MontgomeryPoint {
+    fn from(affine: NonIdentity<MontgomeryPoint>) -> Self {
+        affine.to_point()
+    }
+}
+
+/// The constant-time alternative is available at [`NonIdentity::new()`].
+impl TryFrom<MontgomeryPoint> for NonIdentity<MontgomeryPoint> {
+    type Error = Error;
+
+    fn try_from(affine_point: MontgomeryPoint) -> Result<Self, Error> {
+        NonIdentity::new(affine_point).into_option().ok_or(Error)
     }
 }
 
@@ -230,10 +277,181 @@ impl From<ProjectiveMontgomeryPoint> for MontgomeryXpoint {
     }
 }
 
+impl DefaultIsZeroes for ProjectiveMontgomeryPoint {}
+
+impl<const N: usize> LinearCombination<[(ProjectiveMontgomeryPoint, MontgomeryScalar); N]>
+    for ProjectiveMontgomeryPoint
+{
+}
+
+impl LinearCombination<[(ProjectiveMontgomeryPoint, MontgomeryScalar)]>
+    for ProjectiveMontgomeryPoint
+{
+}
+
+impl CofactorGroup for ProjectiveMontgomeryPoint {
+    type Subgroup = ProjectiveMontgomeryPoint;
+
+    fn clear_cofactor(&self) -> Self::Subgroup {
+        self.double().double()
+    }
+
+    fn into_subgroup(self) -> CtOption<Self::Subgroup> {
+        CtOption::new(self.clear_cofactor(), self.is_torsion_free())
+    }
+
+    fn is_torsion_free(&self) -> Choice {
+        (self * MontgomeryScalar::new(ORDER)).ct_eq(&Self::IDENTITY)
+    }
+}
+
+impl Group for ProjectiveMontgomeryPoint {
+    type Scalar = MontgomeryScalar;
+
+    fn try_from_rng<R>(_rng: &mut R) -> Result<Self, R::Error>
+    where
+        R: TryRngCore + ?Sized,
+    {
+        todo!()
+    }
+
+    fn identity() -> Self {
+        Self::IDENTITY
+    }
+
+    fn generator() -> Self {
+        Self::GENERATOR
+    }
+
+    fn is_identity(&self) -> Choice {
+        self.ct_eq(&Self::IDENTITY)
+    }
+
+    // See Complete Addition Law for Montgomery Curves - Algorithm 3.
+    // Slightly corrected from the derivation in the same paper.
+    fn double(&self) -> Self {
+        const A_MINUS_1: FieldElement = FieldElement(ConstMontyType::new(&U448::from_u64(156325)));
+
+        let (x, y, z) = (self.U, self.V, self.W);
+
+        let t0 = x.square();
+        let t1 = y.square();
+        let t2 = z.square();
+        let t3 = (x + y).square();
+        let t4 = (y + z).square();
+        let t5 = (x + z).square();
+        let t6 = t1 + t2;
+        let t7 = (t0 - t2).double();
+        let t8 = A_MINUS_1 * t0;
+        let t9 = t0 - t1;
+        let t10 = FieldElement::J * (t5 - t2) + t0 + t9;
+        let t11 = t5 + t8;
+        let t13 = t6.double(); // corrected - replaces t12
+
+        let S_MINUS_U = t3 - t6;
+        let S_PLUS_U = -S_MINUS_U + t7;
+        let R_MINUS_T = t4 - t11;
+        let R_PLUS_T = t4 - t13 + t11; // corrected
+        let W_MINUS_S = t11 - t9;
+        let W_PLUS_S = W_MINUS_S + t7;
+        let T_MINUS_V = t11 - t10 - t13 + t8; // corrected
+        let T_PLUS_V = t5 + t10;
+        let U_MINUS_W = S_PLUS_U - W_PLUS_S;
+        let R_PLUS_V = R_MINUS_T + T_PLUS_V;
+
+        let C = R_PLUS_T * S_MINUS_U;
+        let D = R_MINUS_T * S_PLUS_U;
+        let E = T_PLUS_V * W_MINUS_S;
+        let F = T_MINUS_V * W_PLUS_S;
+        let X = C + D;
+        let Y = E + F;
+        let Z = U_MINUS_W.double() * R_PLUS_V + C - D + E - F;
+
+        Self { U: X, V: Y, W: Z }
+    }
+}
+
+impl CurveGroup for ProjectiveMontgomeryPoint {
+    type AffineRepr = MontgomeryPoint;
+
+    fn to_affine(&self) -> Self::AffineRepr {
+        let W_inv = self.W.invert();
+        let x = self.U * W_inv;
+        let y = self.V * W_inv;
+
+        MontgomeryPoint { x, y }
+    }
+}
+
+impl GroupEncoding for ProjectiveMontgomeryPoint {
+    type Repr = Array<u8, U57>;
+
+    fn from_bytes(bytes: &Self::Repr) -> CtOption<Self> {
+        // Safe to unwrap here as the underlying data structure is an array
+        let (tag, bytes) = bytes.split_first().expect("slice is non-empty");
+
+        let mut x_bytes: [u8; 56] = [0; 56];
+        x_bytes.copy_from_slice(bytes);
+
+        let (sign, valid) = match *tag {
+            0x02 => (Choice::from(0), Choice::from(1)),
+            0x03 => (Choice::from(1), Choice::from(1)),
+            _ => (Choice::from(0), Choice::from(0)),
+        };
+
+        FieldElement::from_repr(&x_bytes).and_then(|x| {
+            CtOption::new(
+                ProjectiveMontgomeryXpoint {
+                    U: x,
+                    W: FieldElement::ONE,
+                }
+                .to_extended(sign),
+                valid,
+            )
+        })
+    }
+
+    fn from_bytes_unchecked(bytes: &Self::Repr) -> CtOption<Self> {
+        // No unchecked conversion possible for compressed points
+        Self::from_bytes(bytes)
+    }
+
+    fn to_bytes(&self) -> Self::Repr {
+        let affine = self.to_affine();
+        let mut compressed_bytes = Array::default();
+
+        compressed_bytes[0] = if affine.y.is_negative().unwrap_u8() == 1 {
+            0x03
+        } else {
+            0x02
+        };
+
+        compressed_bytes[1..].copy_from_slice(&affine.x.to_bytes()[..]);
+        compressed_bytes
+    }
+}
+
+impl PrimeGroup for ProjectiveMontgomeryPoint {}
+
+impl From<NonIdentity<ProjectiveMontgomeryPoint>> for ProjectiveMontgomeryPoint {
+    fn from(affine: NonIdentity<ProjectiveMontgomeryPoint>) -> Self {
+        affine.to_point()
+    }
+}
+
+/// The constant-time alternative is available at [`NonIdentity::new()`].
+impl TryFrom<ProjectiveMontgomeryPoint> for NonIdentity<ProjectiveMontgomeryPoint> {
+    type Error = Error;
+
+    fn try_from(point: ProjectiveMontgomeryPoint) -> Result<Self, Error> {
+        NonIdentity::new(point).into_option().ok_or(Error)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{EdwardsPoint, MontgomeryScalar};
+    use crate::EdwardsPoint;
 
     #[test]
     fn to_edwards() {
