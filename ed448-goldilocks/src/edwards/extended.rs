5a5c671e7c8f02414b63f04e2c72acececcd8c55
daxpedda <daxpedda@gmail.com>
2025-07-20 15:39:41 +0200

    Hide `EDWARDS_BASEPOINT_ORDER`

--- ed448-goldilocks/src/edwards/extended.rs
+++ ed448-goldilocks/src/edwards/extended.rs
@@ -3,7 +3,6 @@ use core::fmt::{Display, Formatter, LowerHex, Result as FmtResult, UpperHex};
 use core::iter::Sum;
 use core::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
-use crate::constants::EDWARDS_BASEPOINT_ORDER;
 use crate::curve::scalar_mul::variable_base;
 use crate::curve::twedwards::extended::ExtendedPoint as TwistedExtendedPoint;
 use crate::field::FieldElement;
@@ -724,7 +723,7 @@ impl EdwardsPoint {
     /// * `false` if `self` has a nonzero torsion component and is not
     ///   in the prime-order subgroup.
     pub fn is_torsion_free(&self) -> Choice {
-        (self * EDWARDS_BASEPOINT_ORDER).ct_eq(&Self::IDENTITY)
+        (self * EdwardsScalar::new(ORDER)).ct_eq(&Self::IDENTITY)
     }
 
     /// Hash a message to a point on the curve
