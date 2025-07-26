912c925bb21804986f83c661990e94a0b50b1232
daxpedda <daxpedda@gmail.com>
2025-07-26 23:57:33 +0200

    Reduce unnecessary conversions to `ExtensiblePoint` and back

--- ed448-goldilocks/src/curve/twedwards/extensible.rs
+++ ed448-goldilocks/src/curve/twedwards/extensible.rs
@@ -1,9 +1,8 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
-use crate::curve::twedwards::{
-    affine::AffineNielsPoint, extended::ExtendedPoint, projective::ProjectiveNielsPoint,
-};
+use crate::curve::twedwards::{affine::AffinePoint, extended::ExtendedPoint};
+use crate::edwards::EdwardsPoint as EdwardsExtendedPoint;
 use crate::field::FieldElement;
 use subtle::{Choice, ConstantTimeEq};
 
@@ -12,6 +11,7 @@ use subtle::{Choice, ConstantTimeEq};
 // Where x = X/Z , y = Y/Z , T1 * T2 = T
 // XXX: I think we have too many point representations,
 // But let's not remove any yet
+#[derive(Copy, Clone, Debug)]
 pub struct ExtensiblePoint {
     pub(crate) X: FieldElement,
     pub(crate) Y: FieldElement,
@@ -34,6 +34,11 @@ impl ConstantTimeEq for ExtensiblePoint {
 impl PartialEq for ExtensiblePoint {
     fn eq(&self, other: &ExtensiblePoint) -> bool {
         self.ct_eq(other).into()
+    }
+}
+impl PartialEq<ExtendedPoint> for ExtensiblePoint {
+    fn eq(&self, other: &ExtendedPoint) -> bool {
+        self.ct_eq(&other.to_extensible()).into()
     }
 }
 impl Eq for ExtensiblePoint {}
@@ -67,99 +72,8 @@ impl ExtensiblePoint {
         }
     }
 
-    /// Adds two extensible points together by converting the other point to a ExtendedPoint
-    pub fn add_extensible(&self, other: &ExtensiblePoint) -> ExtensiblePoint {
-        self.add_extended(&other.to_extended())
-    }
-
-    /// Adds an extensible point to an extended point
-    /// Returns an extensible point
-    /// (3.1) https://iacr.org/archive/asiacrypt2008/53500329/53500329.pdf
-    pub fn add_extended(&self, other: &ExtendedPoint) -> ExtensiblePoint {
-        let A = self.X * other.X;
-        let B = self.Y * other.Y;
-        let C = self.T1 * self.T2 * other.T * FieldElement::TWISTED_D;
-        let D = self.Z * other.Z;
-        let E = (self.X + self.Y) * (other.X + other.Y) - A - B;
-        let F = D - C;
-        let G = D + C;
-        let H = B + A;
-        ExtensiblePoint {
-            X: E * F,
-            Y: G * H,
-            T1: E,
-            T2: H,
-            Z: F * G,
-        }
-    }
-
-    /// Subtracts an extensible point from an extended point
-    /// Returns an extensible point
-    /// This is a direct modification of the addition formula to the negation of `other`
-    pub fn sub_extended(&self, other: &ExtendedPoint) -> ExtensiblePoint {
-        let A = self.X * other.X;
-        let B = self.Y * other.Y;
-        let C = self.T1 * self.T2 * other.T * FieldElement::TWISTED_D;
-        let D = self.Z * other.Z;
-        let E = (self.X + self.Y) * (other.Y - other.X) + A - B;
-        let F = D + C;
-        let G = D - C;
-        let H = B - A;
-        ExtensiblePoint {
-            X: E * F,
-            Y: G * H,
-            T1: E,
-            T2: H,
-            Z: F * G,
-        }
-    }
-
-    /// Adds an extensible point to an AffineNiels point
-    /// Returns an Extensible point
-    pub fn add_affine_niels(&self, other: AffineNielsPoint) -> ExtensiblePoint {
-        let A = other.y_minus_x * (self.Y - self.X);
-        let B = other.y_plus_x * (self.X + self.Y);
-        let C = other.td * self.T1 * self.T2;
-        let D = B + A;
-        let E = B - A;
-        let F = self.Z - C;
-        let G = self.Z + C;
-        ExtensiblePoint {
-            X: E * F,
-            Y: G * D,
-            Z: F * G,
-            T1: E,
-            T2: D,
-        }
-    }
-
-    /// Adds an extensible point to a ProjectiveNiels point
-    /// Returns an extensible point
-    /// (3.1)[Last set of formulas] https://iacr.org/archive/asiacrypt2008/53500329/53500329.pdf
-    /// This differs from the formula above by a factor of 2. Saving 1 Double
-    /// Cost 8M
-    pub fn add_projective_niels(&self, other: &ProjectiveNielsPoint) -> ExtensiblePoint {
-        // This is the only step which makes it different than adding an AffineNielsPoint
-        let Z = self.Z * other.Z;
-
-        let A = (self.Y - self.X) * other.Y_minus_X;
-        let B = (self.Y + self.X) * other.Y_plus_X;
-        let C = other.Td * self.T1 * self.T2;
-        let D = B + A;
-        let E = B - A;
-        let F = Z - C;
-        let G = Z + C;
-        ExtensiblePoint {
-            X: E * F,
-            Y: G * D,
-            Z: F * G,
-            T1: E,
-            T2: D,
-        }
-    }
-
     /// Converts an extensible point to an extended point
-    pub fn to_extended(&self) -> ExtendedPoint {
+    pub fn to_extended(self) -> ExtendedPoint {
         ExtendedPoint {
             X: self.X,
             Y: self.Y,
@@ -168,13 +82,50 @@ impl ExtensiblePoint {
         }
     }
 
-    /// Converts an Extensible point to a ProjectiveNiels Point
-    pub fn to_projective_niels(&self) -> ProjectiveNielsPoint {
-        ProjectiveNielsPoint {
-            Y_plus_X: self.X + self.Y,
-            Y_minus_X: self.Y - self.X,
-            Z: self.Z + self.Z,
-            Td: self.T1 * self.T2 * FieldElement::TWO_TIMES_TWISTED_D,
+    /// Converts an extended point to Affine co-ordinates
+    pub(crate) fn to_affine(self) -> AffinePoint {
+        // Points to consider:
+        // - All points where Z=0, translate to (0,0)
+        // - The identity point has z=1, so it is not a problem
+
+        let INV_Z = self.Z.invert();
+
+        let x = self.X * INV_Z;
+        let y = self.Y * INV_Z;
+
+        AffinePoint { x, y }
+    }
+
+    /// Edwards_Isogeny is derived from the doubling formula
+    /// XXX: There is a duplicate method in the twisted edwards module to compute the dual isogeny
+    /// XXX: Not much point trying to make it generic I think. So what we can do is optimise each respective isogeny method for a=1 or a = -1 (currently, I just made it really slow and simple)
+    fn edwards_isogeny(&self, a: FieldElement) -> EdwardsExtendedPoint {
+        // Convert to affine now, then derive extended version later
+        let affine = self.to_affine();
+        let x = affine.x;
+        let y = affine.y;
+
+        // Compute x
+        let xy = x * y;
+        let x_numerator = xy.double();
+        let x_denom = y.square() - (a * x.square());
+        let new_x = x_numerator * x_denom.invert();
+
+        // Compute y
+        let y_numerator = y.square() + (a * x.square());
+        let y_denom = (FieldElement::ONE + FieldElement::ONE) - y.square() - (a * x.square());
+        let new_y = y_numerator * y_denom.invert();
+
+        EdwardsExtendedPoint {
+            X: new_x,
+            Y: new_y,
+            Z: FieldElement::ONE,
+            T: new_x * new_y,
         }
+    }
+
+    /// Uses a 2-isogeny to map the point to the Ed448-Goldilocks
+    pub fn to_untwisted(self) -> EdwardsExtendedPoint {
+        self.edwards_isogeny(FieldElement::MINUS_ONE)
     }
 }
