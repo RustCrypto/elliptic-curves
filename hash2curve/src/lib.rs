3ce6755f09d4e9f95d5908a28830af8c67784b93
daxpedda <daxpedda@gmail.com>
2025-09-14 17:04:21 +0200

    Move standalone functions to root

--- hash2curve/src/lib.rs
+++ hash2curve/src/lib.rs
@@ -31,3 +31,80 @@ pub use group_digest::*;
 pub use hash2field::*;
 pub use map2curve::*;
 pub use oprf::*;
+
+use elliptic_curve::ProjectivePoint;
+use elliptic_curve::array::typenum::NonZero;
+use elliptic_curve::array::{Array, ArraySize};
+use elliptic_curve::group::cofactor::CofactorGroup;
+use elliptic_curve::ops::Reduce;
+
+/// Computes the hash to curve routine.
+/// See [`GroupDigest::hash_from_bytes()`] for more details.
+///
+/// For the `expand_message` call, `len_in_bytes = <Self::FieldElement as FromOkm>::Length * 2`.
+/// This value must be less than `u16::MAX` or otherwise a compiler error will occur.
+///
+/// # Errors
+///
+/// When the chosen [`ExpandMsg`] implementation returns an error. See [`ExpandMsgXmdError`]
+/// and [`ExpandMsgXofError`] for examples.
+///
+/// [`ExpandMsgXmdError`]: crate::ExpandMsgXmdError
+/// [`ExpandMsgXofError`]: crate::ExpandMsgXofError
+pub fn hash_from_bytes<C, X>(msg: &[&[u8]], dst: &[&[u8]]) -> Result<ProjectivePoint<C>, X::Error>
+where
+    C: MapToCurve,
+    X: ExpandMsg<C::SecurityLevel>,
+{
+    let [u0, u1] = hash_to_field::<2, X, _, C::FieldElement, C::Length>(msg, dst)?;
+    let q0 = C::map_to_curve(u0);
+    let q1 = C::map_to_curve(u1);
+    Ok((q0 + q1).clear_cofactor())
+}
+
+/// Computes the encode to curve routine.
+/// See [`GroupDigest::encode_from_bytes()`] for more details.
+///
+/// For the `expand_message` call, `len_in_bytes = <Self::FieldElement as FromOkm>::Length`.
+///
+/// # Errors
+///
+/// When the chosen [`ExpandMsg`] implementation returns an error. See [`ExpandMsgXmdError`]
+/// and [`ExpandMsgXofError`] for examples.
+///
+/// [`ExpandMsgXmdError`]: crate::ExpandMsgXmdError
+/// [`ExpandMsgXofError`]: crate::ExpandMsgXofError
+pub fn encode_from_bytes<C, X>(msg: &[&[u8]], dst: &[&[u8]]) -> Result<ProjectivePoint<C>, X::Error>
+where
+    C: MapToCurve,
+    X: ExpandMsg<C::SecurityLevel>,
+{
+    let [u] = hash_to_field::<1, X, _, C::FieldElement, C::Length>(msg, dst)?;
+    let q0 = C::map_to_curve(u);
+    Ok(q0.clear_cofactor())
+}
+
+/// Computes the hash to field routine according to
+/// <https://www.rfc-editor.org/rfc/rfc9380.html#section-5-4>
+/// and returns a scalar.
+///   
+/// For the `expand_message` call, `len_in_bytes = <Self::FieldElement as FromOkm>::Length`.
+/// This value must be less than `u16::MAX` or otherwise a compiler error will occur.
+///
+/// # Errors
+///
+/// When the chosen [`ExpandMsg`] implementation returns an error. See [`ExpandMsgXmdError`]
+/// and [`ExpandMsgXofError`] for examples.
+///
+/// [`ExpandMsgXmdError`]: crate::ExpandMsgXmdError
+/// [`ExpandMsgXofError`]: crate::ExpandMsgXofError
+pub fn hash_to_scalar<C, X, L>(msg: &[&[u8]], dst: &[&[u8]]) -> Result<C::Scalar, X::Error>
+where
+    C: MapToCurve,
+    X: ExpandMsg<C::SecurityLevel>,
+    L: ArraySize + NonZero,
+    C::Scalar: Reduce<Array<u8, L>>,
+{
+    let [u] = hash_to_field::<1, X, _, C::Scalar, L>(msg, dst)?;
+    Ok(u)
+}
