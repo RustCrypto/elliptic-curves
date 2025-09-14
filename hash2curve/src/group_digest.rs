4d82666897715f0e23988713b37a3a03246f4402
daxpedda <daxpedda@gmail.com>
2025-09-14 17:04:21 +0200

    Move standalone functions to root

--- hash2curve/src/group_digest.rs
+++ hash2curve/src/group_digest.rs
@@ -1,11 +1,7 @@
 //! Traits for handling hash to curve.
 
-use super::{ExpandMsg, MapToCurve, hash_to_field};
+use crate::{ExpandMsg, MapToCurve};
 use elliptic_curve::ProjectivePoint;
-use elliptic_curve::array::typenum::NonZero;
-use elliptic_curve::array::{Array, ArraySize};
-use elliptic_curve::group::cofactor::CofactorGroup;
-use elliptic_curve::ops::Reduce;
 
 /// Hash arbitrary byte sequences to a valid group element.
 pub trait GroupDigest: MapToCurve {
@@ -40,7 +36,7 @@ pub trait GroupDigest: MapToCurve {
         dst: &[u8],
     ) -> Result<ProjectivePoint<Self>, <Self::ExpandMsg as ExpandMsg<Self::SecurityLevel>>::Error>
     {
-        hash_from_bytes::<Self, Self::ExpandMsg>(&[msg], &[dst])
+        crate::hash_from_bytes::<Self, Self::ExpandMsg>(&[msg], &[dst])
     }
 
     /// Computes the encode to curve routine.
@@ -65,77 +61,6 @@ pub trait GroupDigest: MapToCurve {
         dst: &[u8],
     ) -> Result<ProjectivePoint<Self>, <Self::ExpandMsg as ExpandMsg<Self::SecurityLevel>>::Error>
     {
-        encode_from_bytes::<Self, Self::ExpandMsg>(&[msg], &[dst])
+        crate::encode_from_bytes::<Self, Self::ExpandMsg>(&[msg], &[dst])
     }
-}
-
-/// Computes the hash to curve routine.
-/// See [`GroupDigest::hash_from_bytes()`] for more details.
-///
-/// For the `expand_message` call, `len_in_bytes = <Self::FieldElement as FromOkm>::Length * 2`.
-/// This value must be less than `u16::MAX` or otherwise a compiler error will occur.
-///
-/// # Errors
-///
-/// When the chosen [`ExpandMsg`] implementation returns an error. See [`ExpandMsgXmdError`]
-/// and [`ExpandMsgXofError`] for examples.
-///
-/// [`ExpandMsgXmdError`]: crate::ExpandMsgXmdError
-/// [`ExpandMsgXofError`]: crate::ExpandMsgXofError
-pub fn hash_from_bytes<C, X>(msg: &[&[u8]], dst: &[&[u8]]) -> Result<ProjectivePoint<C>, X::Error>
-where
-    C: MapToCurve,
-    X: ExpandMsg<C::SecurityLevel>,
-{
-    let [u0, u1] = hash_to_field::<2, X, _, C::FieldElement, C::Length>(msg, dst)?;
-    let q0 = C::map_to_curve(u0);
-    let q1 = C::map_to_curve(u1);
-    Ok((q0 + q1).clear_cofactor())
-}
-
-/// Computes the encode to curve routine.
-/// See [`GroupDigest::encode_from_bytes()`] for more details.
-///
-/// For the `expand_message` call, `len_in_bytes = <Self::FieldElement as FromOkm>::Length`.
-///
-/// # Errors
-///
-/// When the chosen [`ExpandMsg`] implementation returns an error. See [`ExpandMsgXmdError`]
-/// and [`ExpandMsgXofError`] for examples.
-///
-/// [`ExpandMsgXmdError`]: crate::ExpandMsgXmdError
-/// [`ExpandMsgXofError`]: crate::ExpandMsgXofError
-pub fn encode_from_bytes<C, X>(msg: &[&[u8]], dst: &[&[u8]]) -> Result<ProjectivePoint<C>, X::Error>
-where
-    C: MapToCurve,
-    X: ExpandMsg<C::SecurityLevel>,
-{
-    let [u] = hash_to_field::<1, X, _, C::FieldElement, C::Length>(msg, dst)?;
-    let q0 = C::map_to_curve(u);
-    Ok(q0.clear_cofactor())
-}
-
-/// Computes the hash to field routine according to
-/// <https://www.rfc-editor.org/rfc/rfc9380.html#section-5-4>
-/// and returns a scalar.
-///   
-/// For the `expand_message` call, `len_in_bytes = <Self::FieldElement as FromOkm>::Length`.
-/// This value must be less than `u16::MAX` or otherwise a compiler error will occur.
-///
-/// # Errors
-///
-/// When the chosen [`ExpandMsg`] implementation returns an error. See [`ExpandMsgXmdError`]
-/// and [`ExpandMsgXofError`] for examples.
-///
-/// [`ExpandMsgXmdError`]: crate::ExpandMsgXmdError
-/// [`ExpandMsgXofError`]: crate::ExpandMsgXofError
-pub fn hash_to_scalar<C, X, L>(msg: &[&[u8]], dst: &[&[u8]]) -> Result<C::Scalar, X::Error>
-where
-    C: MapToCurve,
-    X: ExpandMsg<C::SecurityLevel>,
-    L: ArraySize + NonZero,
-    C::Scalar: Reduce<Array<u8, L>>,
-{
-    let [u] = hash_to_field::<1, X, _, C::Scalar, L>(msg, dst)?;
-    Ok(u)
 }
