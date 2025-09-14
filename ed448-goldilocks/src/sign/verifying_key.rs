187f08d3c0a9ca54a4a17900e2d56a6d339aeb97
radik878 <radikpadik76@gmail.com>
2025-09-14 22:27:49 +0300

    Update verifying_key.rs

--- ed448-goldilocks/src/sign/verifying_key.rs
+++ ed448-goldilocks/src/sign/verifying_key.rs
@@ -279,13 +279,9 @@ impl VerifyingKey {
             return Err(SigningError::InvalidSignatureSComponent.into());
         }
 
-        // RFC 8032 mandates context length <= 255 bytes. Enforce consistently with signing path.
-        if ctx.len() > 255 {
-            return Err(SigningError::PrehashedContextLength.into());
-        }
-
         // SHAKE256(dom4(F, C) || R || A || PH(M), 114) -> scalar k
         let mut bytes = WideEdwardsScalarBytes::default();
+        // RFC 8032 mandates context length <= 255 bytes. Enforce consistently with signing path.
         let ctx_len = u8::try_from(ctx.len()).map_err(|_| SigningError::PrehashedContextLength)?;
         let mut reader = Shake256::default()
             .chain(HASH_HEAD)
