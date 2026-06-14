22c745a38e87f93dd55d097e9b1f6b6b1ffb1205
Tony Arcieri <bascule@gmail.com>
2026-06-13 18:33:15 -0600

    primeorder: add `BasepointTable::mul`
    Adds a generic implementation of constant-time scalar multiplication
    which uses `BasepointTable` for acceleration, adapted from the
    implementation in `k256` and made generic.
    
    This also defines a `BASEPOINT_TABLE` constant in `p256` which is used
    to test `BasepointTable::mul`, although it's not actually used to
    accelerate scalar multiplication in the crate's public API yet.

--- p256/src/arithmetic/tables.rs
+++ p256/src/arithmetic/tables.rs
@@ -3,6 +3,23 @@
 #[cfg(feature = "alloc")]
 pub(super) use vartime::BASEPOINT_TABLE_VARTIME;
 
+use super::NistP256;
+use crate::ProjectivePoint;
+use primeorder::PrimeCurveWithBasepointTable;
+
+/// Window size for the basepoint table.
+const WINDOW_SIZE: usize = 33;
+
+/// Basepoint table for multiples of secp256r1's generator.
+type BasepointTable = primeorder::BasepointTable<ProjectivePoint, WINDOW_SIZE>;
+
+/// Lazily computed basepoint table.
+pub(super) static BASEPOINT_TABLE: BasepointTable = BasepointTable::new();
+
+impl PrimeCurveWithBasepointTable<WINDOW_SIZE> for NistP256 {
+    const BASEPOINT_TABLE: &'static BasepointTable = &BASEPOINT_TABLE;
+}
+
 #[cfg(feature = "alloc")]
 mod vartime {
     use crate::{NistP256, ProjectivePoint};
@@ -20,5 +37,33 @@ mod vartime {
 
     impl PrimeCurveWithBasepointTableVartime<WINDOW_SIZE_VARTIME> for NistP256 {
         const BASEPOINT_TABLE_VARTIME: &'static BasepointTableVartime = &BASEPOINT_TABLE_VARTIME;
+    }
+}
+
+/// These are the main tests for `primeorder::BasepointTable` as we need a concrete curve to test
+/// against.
+#[cfg(test)]
+mod tests {
+    use super::BASEPOINT_TABLE;
+    use crate::{ProjectivePoint, Scalar};
+    use elliptic_curve::{
+        array::{Array, sizes::U32},
+        ops::Reduce,
+    };
+    use proptest::prelude::*;
+
+    prop_compose! {
+        fn scalar()(bytes in any::<[u8; 32]>()) -> Scalar {
+            Scalar::reduce(&Array::<u8, U32>::from(bytes))
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn basepoint_table_mul(x in scalar()) {
+            let expected = ProjectivePoint::GENERATOR * &x;
+            let actual = BASEPOINT_TABLE.mul(&x);
+            prop_assert_eq!(expected, actual);
+        }
     }
 }
