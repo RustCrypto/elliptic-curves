3f1b528d6ee249f7f25b3a077921b5322c20c5d3
Tony Arcieri <bascule@gmail.com>
2026-07-06 11:03:45 -0600

    p521: lint improvements (#1895)

--- p384/src/lib.rs
+++ p384/src/lib.rs
@@ -1,13 +1,10 @@
 #![no_std]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
 )]
-#![forbid(unsafe_code)]
-#![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
-#![doc = include_str!("../README.md")]
-
 //! ## Backends
 //!
 //! This crate has support for two different field arithmetic backends which can be selected using
